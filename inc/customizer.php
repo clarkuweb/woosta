e5a92abce0b42a579e99417e5538242b48000740
John Pennypacker <john@pennypacker.net>
2022-11-23 09:14:43 -0500

    removes colors and bg image sections from customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -5,6 +5,21 @@
  * @package woosta
  */
 
+
+function woosta_remove_sections( $wp_customize ) {
+
+	//$wp_customize->remove_section('header_image');
+	//$wp_customize->remove_panel('nav_menus');
+	//$wp_customize->remove_panel('widgets');
+	//$wp_customize->remove_section('custom_css');	
+	$wp_customize->remove_section('colors');
+	$wp_customize->remove_section('background_image');
+	//$wp_customize->remove_section('static_front_page');	 
+	//$wp_customize->remove_section('title_tagline');	
+}
+add_action( 'customize_register', 'woosta_remove_sections');
+
+
 /**
  * Add postMessage support for site title and description for the Theme Customizer.
  *
