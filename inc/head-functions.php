04bbe4861e76ba96c105e80ab8ea014f65a68251
John Pennypacker <john@pennypacker.net>
2022-12-19 15:29:24 -0500

    adds GTM code

--- inc/head-functions.php
+++ inc/head-functions.php
@@ -9,10 +9,10 @@
  * Adds Google Tag Manager code to <head>
  */
 function woosta_gtm() {
-	$gtm = '';
+	$gtm = 'GTM-WMPL9S';
 	echo "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','" . $gtm . "');</script>";
 }
-// add_action( 'wp_head', 'woosta_gtm' );
+add_action( 'wp_head', 'woosta_gtm' );
 
 
 
