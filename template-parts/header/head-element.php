3642e657ec0551eb8b9e4665b85f3dc52d4aec80
John Pennypacker <john@pennypacker.net>
2023-01-06 16:06:15 -0500

    adds favicon assets

---
+++ template-parts/header/head-element.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template part for the contents of the <head> element
+ */
+?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/media/icons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/media/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/media/icons/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/media/icons/site.webmanifest">
+	<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/media/icons/safari-pinned-tab.svg" color="#cc0000">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="theme-color" content="#ffffff">
+
+
+	<?php wp_head(); ?>
