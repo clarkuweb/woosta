4244c8d7098dc31699f087b7fbc0ca713943a1cc
John Pennypacker <john@pennypacker.net>
2023-02-06 16:17:09 -0500

    makes people and container styles more responsive

--- inc/customizer.php
+++ inc/customizer.php
@@ -12,8 +12,8 @@ function woosta_remove_sections( $wp_customize ) {
 	//$wp_customize->remove_panel('nav_menus');
 	//$wp_customize->remove_panel('widgets');
 	//$wp_customize->remove_section('custom_css');	
-	$wp_customize->remove_section('colors');
-	$wp_customize->remove_section('background_image');
+ 	$wp_customize->remove_section('colors');
+ 	$wp_customize->remove_section('background_image');
 	//$wp_customize->remove_section('static_front_page');	 
 	//$wp_customize->remove_section('title_tagline');	
 }
